f2bb71e3c7e6d13c1bcf778a2646b6c2f99f7d67
Mountainbrussells <brussell@Bens-MacBook-Pro.local>
2015-08-20 20:42:27 -0600

    Initial Commit

---
+++ Simone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simone
+//
+//  Created by Ben Russell on 8/20/15.
+//  Copyright (c) 2015 Ben Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
